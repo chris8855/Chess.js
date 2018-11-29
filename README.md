b2fecc3ed507cb9d1d44c1fb5581ae9773477587
MartinSkatvedt <32820275+MartinSkatvedt@users.noreply.github.com>
2018-11-29 12:42:04 +0100

    added castling

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ v1.1
 
 TO DO:
 1. Castling as a legal move
-2. List of moves made
-3. Accounts
-4. Online multiplayer
-5. 1 player AI
+2. En passant
+3. List of moves made
+4. Accounts
+5. Online multiplayer
+6. 1 player AI
--- controls.js
+++ controls.js
@@ -4,9 +4,7 @@ function mousePressed() {
     p = findPiece(currentCell);
     if ((order == 0 && p != 0) && ((p.color != "b" && tur == "white") || (p.color != "w" && tur == "black"))) newPiece();
     else if ((order == 1 && p != 0) && ((p.color == "w" && tur == "white") || (p.color == "b" && tur == "black"))){
-      if ((prevP instanceof King && p instanceof Rook) || (prevP instanceof Rook && p instanceof King)) {
-        console.log(castle(prevP, p));
-      }
+      if ((prevP instanceof King && p instanceof Rook) || (prevP instanceof Rook && p instanceof King)) castle(prevP, p);
       else newPiece();
     }
     else if ((order == 1) && (p == 0)) move();
